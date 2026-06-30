a07b7bcd40a76ccff9275c9fc0dadc91b466c324
DMustache <dvusakov@gmail.com>
2026-06-30 07:19:57 +0000

    chore(repo): initialize commit conventions

---
+++ commitlint.config.cjs
@@ -0,0 +1,52 @@
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "type-enum": [
+            2,
+            "always",
+            [
+                "feat",
+                "fix",
+                "docs",
+                "refactor",
+                "perf",
+                "test",
+                "build",
+                "ci",
+                "chore",
+                "style",
+                "revert",
+            ],
+        ],
+
+        "scope-enum": [
+            1,
+            "always",
+            [
+                "repo",
+                "quartz",
+                "content",
+                "agents",
+                "scripts",
+                "templates",
+                "rust",
+                "python",
+                "backend",
+                "architecture",
+                "engineering",
+                "factorio",
+                "sources",
+                "projects",
+                "ci",
+                "deps",
+                "release",
+            ],
+        ],
+
+        "scope-case": [2, "always", "kebab-case"],
+        "subject-case": [0],
+        "subject-empty": [2, "never"],
+        "subject-full-stop": [2, "never", "."],
+        "header-max-length": [2, "always", 100],
+    },
+};
